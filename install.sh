1a5fe1e8bb77dbd575f0fe6f5e4660154918b7ae
Omar Bheda <35583758+obheda12@users.noreply.github.com>
2021-02-21 17:39:35 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+# Install jq to parse JSON - You need python3 or pip to install it this way - otherwise you may install it manually
+pip3 install jq
+
+# Install haktldextract - Requires GO, install GO if you have not already
+go get github.com/hakluke/haktldextract
